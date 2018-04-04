9e56cb2aecee1e6e9db105d5d7250d6c7cc53572
JOSEEDUARDO <jec_313_vvkr@hotmail.com>
2018-04-03 21:43:31 -0500

    Primer commit 03/04/2018

---
+++ src/main/java/proyecto/hdp/Mensaje.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.hdp;
+
+/**
+ *
+ * @author JOSEEDUARDO
+ */
+public class Mensaje {
+    
+    private String contenido;
+    
+    public Mensaje() {
+    }
+
+    public Mensaje(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+    
+}
---
+++ src/main/java/proyecto/hdp/Usuario.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.hdp;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author JOSEEDUARDO
+ */
+public class Usuario {
+    
+    private String id;
+    private String nickname;
+    private String email;
+    private String pasword;
+    private ArrayList<Mensaje> mensaje;
+    private Posicion posicion;
+
+    public Usuario() {
+    }
+
+    public Usuario(String id, String nickname, String email, String pasword, ArrayList<Mensaje> mensaje, Posicion posicion) {
+        this.id = id;
+        this.nickname = nickname;
+        this.email = email;
+        this.pasword = pasword;
+        this.mensaje = mensaje;
+        this.posicion = posicion;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasword() {
+        return pasword;
+    }
+
+    public void setPasword(String pasword) {
+        this.pasword = pasword;
+    }
+
+    public ArrayList<Mensaje> getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(ArrayList<Mensaje> mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+}
